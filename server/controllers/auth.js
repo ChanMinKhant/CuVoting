a0966a2bfa2eb1a2144873ac87bf7191956c9b12
ChanMinKhant <jiyonfire@gmail.com>
2025-01-03 20:29:51 +0630

    Refactor registration logic to improve error handling and OTP generation process

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -13,10 +13,6 @@ const {
 } = require('../validate/auth');
 
 exports.register = asyncHandler(async (req, res, next) => {
-  // if (req.user) {
-  //   const err = new CustomError('You are already logged in', 400);
-  //   return next(err);
-  // }
   const {
     email,
     password,
@@ -26,77 +22,76 @@ exports.register = asyncHandler(async (req, res, next) => {
     year,
     deviceId,
   } = req.body;
-  console.log(req.body);
-  const { error } = registerSchema.validate(req.body);
-  console.log(error);
-  if (error) {
-    const err = new CustomError(error.details[0].message, 400);
-    return next(err);
-  }
-  let user = await User.findOne({ email });
-  const isPending = await Otp.find({ email });
-  if (user?.isVerified) {
-    const err = new CustomError('User already exists', 400);
-    return next(err);
-  }
 
-  if (user || isPending.length > 0) {
-    await Otp.deleteMany({ email });
-    user.password = password;
-    user.deviceId = deviceId;
-    user.username = username;
-    // user.section = section;
-    // user.year = year;
-    // user.major = major;
-    // user.occupation = occupation;
-    await user.save();
-  } else {
-    user = await User.create({
-      email,
-      password,
-      deviceId,
-    });
+  console.log('Register Request Body:', req.body);
+
+  // Validate input
+  const { error } = registerSchema.validate(req.body);
+  if (error) {
+    return next(new CustomError(error.details[0].message, 400));
   }
 
   if (password !== confirmPassword) {
-    const err = new CustomError('Passwords do not match', 400);
-    return next(err);
+    return next(new CustomError('Passwords do not match', 400));
   }
 
-  //send otp
-  const otp = Math.floor(100000 + Math.random() * 900000);
-  console.log(otp);
-
-  // hash the otp
-  const hashOtp = bcrypt.hashSync(otp.toString(), 10);
-  const otpData = {
-    otp: hashOtp,
-    email: email,
-  };
-
-  const otpDoc = await Otp.create(otpData);
-  if (!otpDoc) {
-    await user.deleteOne();
-    const err = new CustomError('Something went wrong', 500);
-    return next(err);
-  }
-
-  //send otp here
-  const message = `Your OTP is ${otp}`;
   try {
-    // const subject = 'OTP for email verification';
-    // await sendMail(email, subject, message);
-  } catch (error) {
-    await user.deleteOne();
-    await otpDoc.deleteOne();
-    console.log(error);
-    const err = new CustomError('Email could not be sent', 500);
-    return next(err);
+    // Check if user exists
+    let user = await User.findOne({ email });
+    const isPending = await Otp.find({ email });
+
+    if (user?.isVerified) {
+      return next(new CustomError('User already exists', 400));
+    }
+
+    if (user || isPending.length > 0) {
+      await Otp.deleteMany({ email });
+      user.password = password;
+      user.deviceId = deviceId;
+      user.username = username;
+      await user.save();
+    } else {
+      user = await User.create({
+        email,
+        password,
+        deviceId,
+      });
+    }
+
+    // Generate OTP
+    const otp = Math.floor(100000 + Math.random() * 900000);
+    console.log('Generated OTP:', otp);
+
+    // Hash the OTP
+    const hashOtp = bcrypt.hashSync(otp.toString(), 10);
+    const otpData = { otp: hashOtp, email };
+
+    const otpDoc = await Otp.create(otpData);
+    if (!otpDoc) {
+      await user.deleteOne();
+      return next(new CustomError('Something went wrong', 500));
+    }
+
+    // Send OTP email
+    const message = `Your OTP is ${otp}`;
+    try {
+      const subject = 'OTP for email verification';
+      await sendMail(email, subject, message);
+    } catch (mailError) {
+      console.error('Mail Error:', mailError);
+      await user.deleteOne();
+      await otpDoc.deleteOne();
+      return next(new CustomError('Email could not be sent', 500));
+    }
+
+    res.status(200).json({
+      success: true,
+      message: 'OTP has been sent to your email.',
+    });
+  } catch (err) {
+    console.error('Register Error:', err);
+    return next(new CustomError('Internal server error', 500));
   }
-  res.status(200).send({
-    success: true,
-    message: 'OTP has been sent to your email.',
-  });
 });
 
 exports.login = asyncHandler(async (req, res, next) => {
