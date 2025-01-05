fbf83d142caf21d3412b7fe02d4f7c38d44f7360
ChanMinKhant <jiyonfire@gmail.com>
2025-01-05 16:37:10 +0630

    Update API base URL to production environment

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -3,8 +3,12 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const apiService = axios.create({
-  baseURL: 'http://localhost:3000',
-  // baseURL: 'https://cu-voting.vercel.app',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'https://cu-voting.vercel.app',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
 });
 
 export default apiService;
