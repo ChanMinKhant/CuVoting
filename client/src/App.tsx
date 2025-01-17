d649ce33755563a7559c81bde8ce9ad52a7e8da1
ChanMinKhant <jiyonfire@gmail.com>
2025-01-17 21:20:56 +0630

    set application to maintenance mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -60,7 +60,7 @@ Tap "Okay" to logout! 👉🚪`;
     checkBanStatus();
   }, [userStatus, user?.user?.isBanned, user?.user?.username]);
 
-  const isMaintaining: boolean = false;
+  const isMaintaining: boolean = true;
   if (isMaintaining) {
     return <MaintenancePage />;
   }
