6a9714a7c6b9965c8833ad38eec046f82fb3d37a
ChanMinKhant <jiyonfire@gmail.com>
2025-01-19 09:57:23 +0630

    feat: update MaintenancePage messaging for improved user experience during upgrades

--- client/src/pages/hero/MaintenancePage.tsx
+++ client/src/pages/hero/MaintenancePage.tsx
@@ -3,17 +3,20 @@ const MaintenancePage = () => {
     <div className='h-screen bg-gradient-to-r from-pink-400 via-purple-500 to-indigo-600 flex justify-center items-center'>
       <div className='text-center max-w-2xl px-6'>
         <h1 className='text-5xl font-bold text-white mb-4 animate__animated animate__fadeIn'>
-          Our website is dating... <br />
+          Excuse Us for a Moment,
+          <br />
           💖💻
         </h1>
         <p className='text-xl text-white mb-6 animate__animated animate__fadeIn animate__delay-1s'>
-          She's busy right now. 🤫
+          Our website is currently engaged in an important upgrade. 🤫
         </p>
         <p className='text-lg text-white mb-8 animate__animated animate__fadeIn animate__delay-2s'>
-          Please don’t disturb her, she’ll be back after dating, with full
-          energy! 🤗🔋
+          Kindly allow us some time to enhance your experience. Rest assured,
+          we’ll return stronger, sharper, and full of energy! 🤗🔋
         </p>
-        <p className='text-lg text-white mb-10 animate__animated animate__fadeIn animate__delay-3s'></p>
+        <p className='text-lg text-white mb-10 animate__animated animate__fadeIn animate__delay-3s'>
+          Thank you for your patience and understanding. 🕴️✨
+        </p>
       </div>
     </div>
   );
