f843c56b798de04128ede7de927db7c6e87d2faa
Chan Min Khant <100338187+ChanMinKhant@users.noreply.github.com>
2025-01-20 19:23:23 +0630

    Update MaintenancePage.tsx

--- client/src/pages/hero/MaintenancePage.tsx
+++ client/src/pages/hero/MaintenancePage.tsx
@@ -3,19 +3,18 @@ const MaintenancePage = () => {
     <div className='h-screen bg-gradient-to-r from-pink-400 via-purple-500 to-indigo-600 flex justify-center items-center'>
       <div className='text-center max-w-2xl px-6'>
         <h1 className='text-5xl font-bold text-white mb-4 animate__animated animate__fadeIn'>
-          Excuse Us for a Moment,
+          ခနလေးနော်
           <br />
-          💖💻
+         
         </h1>
         <p className='text-xl text-white mb-6 animate__animated animate__fadeIn animate__delay-1s'>
-          Our website is currently engaged in an important upgrade. 🤫
+          မီးပျက်နေလို့ပါခင်ဗျာ။ 😁
         </p>
         <p className='text-lg text-white mb-8 animate__animated animate__fadeIn animate__delay-2s'>
-          Kindly allow us some time to enhance your experience. Rest assured,
-          we’ll return stronger, sharper, and full of energy! 🤗🔋
+          ခနလောက်စောင့်ကြပါဦး 🤣
         </p>
         <p className='text-lg text-white mb-10 animate__animated animate__fadeIn animate__delay-3s'>
-          Thank you for your patience and understanding. 🕴️✨
+          😸😸
         </p>
       </div>
     </div>
