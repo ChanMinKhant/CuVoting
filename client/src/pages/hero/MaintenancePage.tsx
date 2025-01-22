85ec2121feb8b550a5781ffe83dd2bd239d97da9
Chan Min Khant <100338187+ChanMinKhant@users.noreply.github.com>
2025-01-22 19:12:40 +0630

    Update MaintenancePage.tsx

--- client/src/pages/hero/MaintenancePage.tsx
+++ client/src/pages/hero/MaintenancePage.tsx
@@ -25,16 +25,16 @@ const MaintenancePage = () => {
     <div className='h-screen bg-gradient-to-r from-pink-400 via-purple-500 to-indigo-600 flex justify-center items-center'>
       <div className='text-center max-w-2xl px-6'>
         <h1 className='text-5xl font-bold text-white mb-4 animate__animated animate__fadeIn'>
-          We're currently fixing things up! 🛠️
+          Well done! 
         </h1>
         <p className='text-2xl text-white mb-4 animate__animated animate__fadeIn animate__delay-1s'>
-          Meanwhile, enjoy this random selection:
+          
         </p>
         <p className='text-3xl font-semibold bg-gradient-to-r from-yellow-300 via-yellow-400 to-yellow-500 text-transparent bg-clip-text mb-6 animate__animated animate__fadeIn animate__delay-1s'>
-          {randomSelection}
+          
         </p>
         <p className='text-lg text-white mb-8 animate__animated animate__fadeIn animate__delay-2s'>
-          Make your choice wisely! Please wait for a moment.
+          
         </p>
         <p className='text-lg text-white mb-10 animate__animated animate__fadeIn animate__delay-3s'>
           👑 👑
